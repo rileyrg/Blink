f3a234ce8f2f36590272af16712d2e5985aa2a4f
Richard Riley <rileyrg@gmail.com>
2019-03-20 15:45:12 +0100

    this can be compiled using PlatformIO or CMake using Arduino-cmake-ng.

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+/**
+ * Blink
+ *
+ * Turns on an LED on for one second,
+ * then off for one second, repeatedly.
+ */
+#include <stdint.h>
+#include <stdio.h>
+
+#define UBRRH
+#include <Arduino.h>
+#include <avr/io.h>
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  long delayPeriod = 500;
+  digitalWrite(LED_BUILTIN,HIGH);
+  delay(delayPeriod);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(delayPeriod);
+}
